7404438e790c3bd377ad5e98632ce73209708752
Nham Le <nle@lilla-my.cs.uwaterloo.ca>
2019-05-01 17:40:28 -0400

    wrapper for loading a ms.CHCProblem

--- metaspacer/utils/utils.py
+++ metaspacer/utils/utils.py
@@ -1,7 +1,10 @@
 import z3
 from ..core import CHCProblem 
 def load(filename, type = None):
-    pass
+    chc = CHCProblem()
+    if type=="smt2":
+        chc.load(filename)
+    return chc
 #    fp = z3.Fixedpoint()
 #    c = CHCProblem()
 #    if type =="datalog":
--- usage.py
+++ usage.py
@@ -1,3 +1,8 @@
 import metaspacer as ms
-f = ms.load("chc-lia-0006.smt2.datalog", type = "datalog")
-print(f)
+
+chc = ms.load('/home/nle/workspace/deepSpacer/chc-lia-0006.smt2', type = "smt2")
+q = ms.Query(chc = chc, text = "( and (= itp_0_n 0) (> itp_1_n 1) )")
+print(q.solve())
+q.update_query("(= itp_2_n 3)")
+print(q.solve())
+
