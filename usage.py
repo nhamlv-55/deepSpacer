5b138714bce970db490bf6a9b63e6869e7ce2647
Nham Le <nle@lilla-my.cs.uwaterloo.ca>
2019-05-01 22:38:24 -0400

    update API in usage.py

--- usage.py
+++ usage.py
@@ -2,7 +2,10 @@ import metaspacer as ms
 
 chc = ms.load('/home/nle/workspace/deepSpacer/chc-lia-0006.smt2', type = "smt2")
 q = ms.Query(chc = chc)
-res, _ = q.execute("( and (= itp_0_n -5) (> itp_0_n 1) )", params = {"verbose": 1})
+print(chc.queries)
+res, _ = q.execute(chc.queries[0], params = {"spacer.max_level": 20})
+print(res)
+res, _ = q.execute("( and (= itp_0_n -5) (> itp_0_n 1) )", params = {"spacer.max_level": 5})
 print("res", res)
 print("get_answer",_.get_answer())
 print("************")
@@ -11,3 +14,4 @@ print(q.query)
 res, f = q.execute("(= itp_3_n 1)")
 print(res)
 print(f.get_ground_sat_answer())
+
