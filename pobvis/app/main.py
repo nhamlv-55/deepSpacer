0aafc21e6edfcbd691e4ce3fe4b8c15dbe6131f7
Nham Le <nle@lilla-my.cs.uwaterloo.ca>
2019-05-04 22:16:57 -0400

    return exception to frontend

--- pobvis/app/main.py
+++ pobvis/app/main.py
@@ -5,7 +5,7 @@ open_log('/tmp/z3log')
 import metaspacer as ms
 
 app = Flask(__name__)
-
+folder = '../../benchmarks/chc-comp18-benchmarks/lia/'
 @app.route("/home/")
 def home():
 
@@ -21,17 +21,30 @@ def send_json(path):
 def execute_file():
     print(request.form)
     filename = request.form["filename"]
+    print(folder + filename)
     query = request.form["query_text"]
+    show_formula = request.form["show_formula"]
     params = json.loads(request.form["params"])
+    
     time = "Now"
     params["spacer.print_json"] = "static/"+filename + "." + time + ".json"
-    chc = ms.load('../../benchmarks/chc-comp18-benchmarks/lia/'+filename, type='smt2')
+    chc = ms.load(folder+filename, type='smt2')
     q = ms.Query(chc)
     if query=="":
         query = chc.queries[0]
     res, _ = q.execute(query, params = params)
-    return jsonify(result = str(res), json_filename = params["spacer.print_json"])
+    if isinstance(res, Exception):
+        debug_mess = str(res.value)
+    else:
+        debug_mess = "ok"
+    if show_formula:
+        print(folder+filename)
+        with open(folder + filename, "r") as f:
+            formula = f.read()
+        return jsonify(debug_mess = debug_mess, result = str(res), json_filename = params["spacer.print_json"], formula = formula)
+    else:
+        return jsonify(debug_mess = debug_mess, result = str(res), json_filename = params["spacer.print_json"])
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug = True, port=8888)
+    app.run(host="0.0.0.0", port=8888)
