362a33ef896de0af7485e99f50a663d3c51513ca
nham le <nv3le@uwaterloo.ca>
2019-05-08 14:57:22 -0400

    fix query using stripQuantifierBlock (borrow from spacer1.1)

--- metaspacer/core/chc_problem.py
+++ metaspacer/core/chc_problem.py
@@ -1,4 +1,4 @@
-from z3 import *
+import z3
 class CHCProblem():
     def __init__(self, filename = None):
         self.filename = filename
@@ -18,23 +18,66 @@ class CHCProblem():
         for i in range(predicate.num_args()):
             var_sort.append(predicate.arg(i).sort())
         return tuple(var_sort)
-    
+
+    def _stripQuantifierBlock (self, expr) :
+        """ strips the outermost quantifier block in a given expression and returns
+        the pair (<list of consts replacing free vars>,
+        <body with consts substituted for de-bruijn variables>)
+
+        Example:
+
+        assume expr.is_forall ()
+        vars, body = strip_quantifier (expr)
+        qexpr = z3.ForAll (vars, body)
+        assert qexpr.eq (expr)
+        """
+        if not z3.is_quantifier (expr) : return ([], expr)
+        z3ctx = expr.ctx
+        consts = list ()
+        # outside-in order of variables; z3 numbers variables inside-out but
+        # substitutes outside-in
+        for i in reversed (range (expr.num_vars ())) :
+            v_name = expr.var_name (i)
+            v_sort = expr.var_sort (i)
+            consts.append (z3.Const (v_name, self._z3_translate (v_sort, z3ctx)))
+        matrix = z3.substitute_vars (expr.body (), *consts)
+        return (consts, matrix)
+
+    def _z3_translate (self, x, ctx):
+        """ A version of z3.AstRef.translate that handles sorts and function declarations correctly"""
+        if x.ctx == ctx: return x
+        if isinstance (x, z3.BoolSortRef): return z3.BoolSort (ctx)
+        if z3.is_arith_sort (x): 
+            if x.is_int (): return z3.IntSort (ctx)
+            else :
+                assert(x.is_real ())
+                return z3.RealSort (ctx)
+        if isinstance (x, z3.FuncDeclRef):
+            sorts = [z3_translate (x.domain (i), ctx) for i in range (decl.arity ())]
+            sorts.append (z3_translate (x.range (), ctx))
+            return z3.Function (x.name (), *sorts)
+
+        return x.translate (ctx)
+
     def load(self, filename):
         self.filename = filename
         print("im about to parse %s"%filename)
-        fs = parse_smt2_file(filename)
+        fs = z3.parse_smt2_file(filename)
         for f in fs:
             # add predicates
             predicate = f.body().arg(1)
-            self.predicates[predicate.decl().name()] = predicate.decl()
-            # add variables and sorts
-            self.all_var_sort[predicate.decl().name()] = self.get_var_sort(predicate)
-            # add queries. Queries are predicate with no argument
             if predicate.num_args()==0:
                 print(predicate.sexpr())
-                self.queries.append(predicate)
-            # add rule
-            self.rules.append(f)
+                vars, body = self._stripQuantifierBlock(f)
+                query = z3.Exists(vars, f.body().arg(0))
+                print("reconstructed query:", query)
+                self.queries.append(query)
+            else:
+                self.predicates[predicate.decl().name()] = predicate.decl()
+                # add variables and sorts
+                self.all_var_sort[predicate.decl().name()] = self.get_var_sort(predicate)
+                # add rule
+                self.rules.append(f)
         print("=======DONE LOADING =======")
 
     def dump(self):
@@ -47,6 +90,6 @@ class CHCProblem():
 
 if __name__ == "__main__":
     chc = CHCProblem()
-    chc.load('/home/nle/workspace/deepSpacer/chc-lia-0006.smt2')
+    chc.load('/home/nv3le/workspace/deepSpacer/benchmarks/chc-comp18-benchmarks/lia/chc-lia-0006.smt2')
     chc.dump()
     
