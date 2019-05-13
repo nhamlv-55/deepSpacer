4e0be1c2c98a55da93f5f6a9dfa214db32bd3c22
nham le <nv3le@uwaterloo.ca>
2019-05-13 16:25:25 -0400

    support dumping and loading lemmas in the interface

--- pobvis/app/main.py
+++ pobvis/app/main.py
@@ -26,27 +26,38 @@ def execute_file():
     show_formula = request.form["show_formula"]
     params = json.loads(request.form["params"])
     level = int(request.form["level"])
+    interactive = request.form["interactive"]
+    lemmas_file = request.form["lemmas_file"]
     time = "Now"
     params["spacer.print_json"] = "static/"+filename + "." + time + ".json"
     chc = ms.CHCProblem(folder+filename)
-    q = ms.Query(chc)
-    if query=="":
-        query = chc.queries[0]
+    if interactive and lemmas_file!="":
+        q = ms.Query(chc, lemmas_file = lemmas_file, params = params)
+    else:
+        q = ms.Query(chc, params = params)
+    if query=="": query = chc.queries[0]
     try:
-        res, _ = q.execute(query, level = level, params = params)
+        res, _ = q.execute(query, level = level)
         json_filename = params["spacer.print_json"]
         status = "OK"
     except Exception as e:
         res = "error"
         json_filename = ""
         status = str(e)
+
+    if interactive:
+        #overwrite old lemmas_file
+        prefix = "Now"
+        lemmas_file = "%s_%s_%sto%s.json"%(prefix, filename, str(level), str(params["spacer.max_level"]))
+        q.dump_lemmas(lemmas_file)
+
     if show_formula:
         print(folder+filename)
         with open(folder + filename, "r") as f:
             formula = f.read()
-        return jsonify(debug_mess = status, result = str(res), json_filename = json_filename, formula = formula)
+        return jsonify(debug_mess = status, result = str(res), json_filename = json_filename, formula = formula, lemmas_file = lemmas_file)
     else:
-        return jsonify(debug_mess = status, result = str(res), json_filename = json_filename)
+        return jsonify(debug_mess = status, result = str(res), json_filename = json_filename, lemmas_file = lemmas_file)
 
 
 if __name__ == "__main__":
