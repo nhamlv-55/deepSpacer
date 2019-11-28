376570d25bebf2eeb621d7814a66f78f5e78dd6e
Nham Van Le <nv3le@precious3.eng.uwaterloo.ca>
2019-11-28 12:41:52 -0500

    fix ordering. Now order in each literal is correct

--- pobvis/app/main_sat_vis.py
+++ pobvis/app/main_sat_vis.py
@@ -16,39 +16,38 @@ from chctools import horndb as H
 import io
 
 import pysmt.operators as pyopt
-
 def calculate_val(node):
     if node.is_real_constant():
         frac = node._content.payload
         val = frac.numerator / frac.denominator
         return val
-    return str(node._content.payload)
-
+    return str(node._content.payload[0])
 def to_json(node):
 #     print(node, node.get_type(), node.args())
+    if node.is_real_constant():
+        type_str = "0_REAL_CONSTANT" #hack to put real constant at the end
+    else:
+        type_str = pyopt.op_to_str(node.node_type())
     if len(node.args())==0:
-        obj = {"type":pyopt.op_to_str(node.node_type()), "content":calculate_val(node)}
+        obj = {"type": type_str, "content":calculate_val(node)}
         return obj
     else:
         args = []
         for a in node.args():
             args.append(to_json(a))
-        obj = {"type":pyopt.op_to_str(node.node_type()), "content": args}
+        obj = {"type": type_str, "content": args}
         return obj
-
-def order_dict(dictionary):
-    result = {}
-    for k, v in sorted(dictionary.items()):
-        if isinstance(v, dict):
-            result[k] = order_dict(v)
-        elif isinstance(v, list):
-            for arg in v:
-                order_dict(arg)
-            sorted(v, key=lambda k: k['type']) 
-            result[k] = v
-        else:
-            result[k] = v
-    return result
+    
+    
+def order_node(node):
+    args = node["content"]
+    if isinstance(args, list):
+        for idx in range(len(args)):
+            args[idx] = order_node(args[idx])
+            
+        args = sorted(args, key=lambda k: k["type"], reverse = True) 
+        node["content"] = args
+    return node
 
 app = Flask(__name__)
 app.config.from_object(__name__)
@@ -116,7 +115,7 @@ def poke():
             expr = node["expr"]
             expr_stream = io.StringIO(expr)
             ast = spacerWrapper.rels[0].pysmt_parse_lemma(expr_stream)
-            ast_json = order_dict(to_json(ast))
+            ast_json = order_node(to_json(ast))
             node["ast_json"] = ast_json
 
 
